02cbdb7dec49085d3bb6577e952b0041d0916d74
NotGael <gael@fontenelle.me>
2017-10-04 16:06:40 +0200

    Create
    Create

---
+++ js/create.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // click on button submit
+    $("#submit").on('click', function(){
+        // send ajax
+        $.ajax({
+            url: 'http://10.10.9.118:8080/animals', // url where to submit the request
+            type : "POST", // type of action POST || GET
+            dataType : 'json', // data type
+            data : $("#contactForm").serialize(), // post data || get data
+            success : function(result) {
+                // you can see the result from the console
+                // tab of the developer tools
+                console.log(result);
+            },
+            error: function(xhr, resp, text) {
+                console.log(xhr, resp, text);
+            }
+        })
+    });
+});
