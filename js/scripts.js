5bfa61eaf0b25b1e727c3f1aceb37e51b2bc1cf2
NotGael <gael@fontenelle.me>
2017-10-04 15:01:24 +0200

    SecondTable

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,23 @@ $.ajax({
         });
     }
 });
+
+$.ajax({
+    type: 'GET',
+    url: 'http://10.10.9.122:8080/animals',
+    data: {},
+    dataType: 'json',
+    success: function (data) {
+        $.each(data, function(index, element) {
+            var txt = "<tr>" + "<td>" + element.id + "</td>" +
+                               "<td>" + element.type + "</td>" +
+                               "<td>" + element.nom + "</td>" +
+                               "<td>" + element.description + "</td>" +
+                               "<td>" + element.cout + "</td>" +
+                               "<td><img src=\"" + element.url + "\" alt=\"Une image d'animal\" style=\"max-width:100px; min-width:100px\"></td>" +
+                               "<td>" + element.dateNaissance + "</td>" +
+                               "</tr>";
+            $('#animalTableBis').append(txt);
+        });
+    }
+});
