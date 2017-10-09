22761b9aa24460a464632d49bde4847e0ffa65fb
NotGael <gael@fontenelle.me>
2017-10-09 10:15:58 +0200

    Net

--- js/scripts.js
+++ js/scripts.js
@@ -58,35 +58,23 @@ $.ajax({
     }
 });
 
-$("#sendMessageButton").click(function() {
-  $(document).ready(function(){
-      // click on button submit
-      $("#submit").on('click', function(){
-          // send ajax
-          $.ajax({
-              url: 'http://10.10.9.118:8080/animals', // url where to submit the request
-              type : "POST", // type of action POST || GET
-              dataType : 'json', // data type
-              data : {
-                type: $("#contactForm input[name=type]").val(),
-                nom: $("#contactForm input[name=nom]").val(),
-                description: $("#contactForm input[name=description]").val(),
-                cout: $("#contactForm input[name=cout]").val(),
-                url: $("#contactForm input[name=url]").val(),
-                dateNaissance: $("#contactForm input[name=dateNaissance]").val()
-              },
-              success : function(result) {
-                  // you can see the result from the console
-                  // tab of the developer tools
-                  console.log(result);
-              },
-              error: function(xhr, resp, text) {
-                  console.log(xhr, resp, text);
-              }
-          })
-      });
-  });
+$.ajax({
+    type: "POST",
+    url: 'http://10.10.9.118:8080/animals',
+    // The key needs to match your method's input parameter (case-sensitive).
+    data: JSON.stringify({
+        type: $("#contactForm input[name=type]").val(),
+        nom: $("#contactForm input[name=nom]").val(),
+        description: $("#contactForm input[name=description]").val(),
+        cout: $("#contactForm input[name=cout]").val(),
+        url: $("#contactForm input[name=url]").val(),
+        dateNaissance: $("#contactForm input[name=dateNaissance]").val()
+    }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+    return false;
 });
-
-
-$("#date").datetimepicker({dateFormat: "dd/mm/yyyy"});
