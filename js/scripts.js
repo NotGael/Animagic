939c9fa376d468a55c6fc378adf7fdf22e381bdd
NotGael <gael@fontenelle.me>
2017-10-09 14:56:42 +0200

    Test

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
+/*
+  DISPLAY ALL
+*/
 $.ajax({
     type: 'GET',
     url: 'http://10.10.9.118:8080/animals',
-    data: {},
-    dataType: 'json',
-    success: function (data) {
+    contentType: "application/json; charset=utf-8",
+    success: function(data) {
+        console.log(data);
         $.each(data, function(index, element) {
             var txt = "<tr>" + "<td>" + element.id + "</td>" +
                                "<td>" + element.type + "</td>" +
@@ -12,64 +15,70 @@ $.ajax({
                                "<td>" + element.cout + "</td>" +
                                "<td><img src=\"" + element.url + "\" alt=\"Une image d'animal\" style=\"max-width:100px; min-width:100px\"></td>" +
                                "<td>" + element.dateNaissance + "</td>" +
+                               "<td><button class=\"btn btn-success btn-lg\" id=\"sendDelete\" data-id=\"" + element.id + "\" >Supprimer</button></td>" +
                                "</tr>";
-            $('#animalTable1').append(txt);
+            console.log(txt);
+            $('#animals').append(txt);
         });
+    },
+    failure: function(errMsg) {
+        alert(errMsg);
     }
 });
-
-$.ajax({
-    type: 'GET',
-    url: 'http://10.10.9.122:8080/animals',
-    data: {},
-    dataType: 'json',
-    success: function (data) {
-        $.each(data, function(index, element) {
-            var txt = "<tr>" + "<td>" + element.id + "</td>" +
-                               "<td>" + element.type + "</td>" +
-                               "<td>" + element.nom + "</td>" +
-                               "<td>" + element.description + "</td>" +
-                               "<td>" + element.cout + "</td>" +
-                               "<td><img src=\"" + element.url + "\" alt=\"Une image d'animal\" style=\"max-width:100px; min-width:100px\"></td>" +
-                               "<td>" + element.dateNaissance + "</td>" +
-                               "</tr>";
-            $('#animalTable2').append(txt);
-        });
-    }
-});
-
-$.ajax({
-    type: 'GET',
-    url: 'http://10.10.9.121:8080/api/animals',
-    data: {},
-    dataType: 'json',
-    success: function (data) {
-        $.each(data, function(index, element) {
-            var txt = "<tr>" + "<td>" + element.id + "</td>" +
-                               "<td>" + element.type + "</td>" +
-                               "<td>" + element.nom + "</td>" +
-                               "<td>" + element.description + "</td>" +
-                               "<td>" + element.cout + "</td>" +
-                               "<td><img src=\"" + element.url + "\" alt=\"Une image d'animal\" style=\"max-width:100px; min-width:100px\"></td>" +
-                               "<td>" + element.dateNaissance + "</td>" +
-                               "</tr>";
-            $('#animalTable3').append(txt);
-        });
-    }
-});
-
-$("#sendMessageButton").click(function() {
+/*
+  CREATE
+*/
+$("#sendCreate").click(function() {
     $.ajax({
         type: "POST",
         url: 'http://10.10.9.118:8080/animals',
         // The key needs to match your method's input parameter (case-sensitive).
         data: JSON.stringify({
-            type: $("#contactForm input[name=type]").val(),
-            nom: $("#contactForm input[name=name]").val(),
-            description: $("#contactForm input[name=description]").val(),
-            cout: $("#contactForm input[name=cout]").val(),
-            url: $("#contactForm input[name=url]").val(),
-            dateNaissance: $("#contactForm input[name=date]").val(),
+            type: $("#createForm input[name=type]").val(),
+            nom: $("#createForm input[name=name]").val(),
+            description: $("#createForm input[name=description]").val(),
+            cout: $("#createForm input[name=cout]").val(),
+            url: $("#createForm input[name=url]").val(),
+            dateNaissance: $("#createForm input[name=date]").val(),
+        }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){alert(data);},
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
+});
+/*
+  DELETE
+*/
+$("#sendDelete").click(function(e) {
+    $.ajax({
+        type: "DELETE",
+        url: 'http://10.10.9.118:8080/animals/'.concat($(this).data("id")).val(),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){alert(data);},
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
+});
+/*
+  UPDATE
+*/
+$("#sendUpdate").click(function(e) {
+    $.ajax({
+        type: "PUT",
+        url: 'http://10.10.9.118:8080/animals'.concat($(this).data("id")).val(),
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify({
+            type: $("#updateForm input[name=type]").val(),
+            nom: $("#updateForm input[name=name]").val(),
+            description: $("#updateForm input[name=description]").val(),
+            cout: $("#updateForm input[name=cout]").val(),
+            url: $("#updateForm input[name=url]").val(),
+            dateNaissance: $("#updateForm input[name=date]").val(),
         }),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
